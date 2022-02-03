cee97d801917b5ec4014b0dab9f871a685a52ef3
Jay <biskyniffer@gmail.com>
2022-02-02 21:46:57 -0500

    added exercise

--- Practice Stuff/ConsoleApp1/Program.cs
+++ Practice Stuff/ConsoleApp1/Program.cs
@@ -6,23 +6,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("Please enter an integer: ");
-            var userInput = Console.ReadLine();
-
-            int guess = Convert.ToInt32(userInput);
-
-            if (guess >= 10)
+            for(var i = 0; i < 20; i++)
             {
-                Console.WriteLine("Wow, that's a big one!!!");
-            }
-            else if (guess > 4 && guess < 10)
-            {
-                Console.WriteLine("That's not quite a smole, but not too big either.");
-            }
-            else
-            {
-                Console.WriteLine("That's a smol.");
-            }
+                Console.WriteLine(i);
+            } 
+            
         }
     }
 }
