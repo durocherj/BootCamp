a842336981dd63afc1089452f8e5cc1bdfb5ecc5
Jay <biskyniffer@gmail.com>
2022-02-02 20:02:07 -0500

    practice

---
+++ Practice Stuff/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter an integer: ");
+            var userInput = Console.ReadLine();
+
+            int guess = Convert.ToInt32(userInput);
+
+            if (guess >= 10)
+            {
+                Console.WriteLine("Wow, that's a big one!!!");
+            }
+            else if (guess > 4 && guess < 10)
+            {
+                Console.WriteLine("That's not quite a smole, but not too big either.");
+            }
+            else
+            {
+                Console.WriteLine("That's a smol.");
+            }
+        }
+    }
+}
