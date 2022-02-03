33febb6d021d30772f4beee294ddf5581fe1caad
Jay <biskyniffer@gmail.com>
2022-02-02 22:05:37 -0500

    exercise 9 with bonus

--- Practice Stuff/Exercise 9/Program.cs
+++ Practice Stuff/Exercise 9/Program.cs
@@ -6,7 +6,47 @@ namespace Exercise_9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            var again = true;
+
+            while (again == true)
+            {
+                Console.WriteLine("Please choose a language from the following: English, French, German, Spanish.");
+                var input = Console.ReadLine();
+                input = input.ToLower();
+
+                switch (input)
+                {
+                case "english":
+                    Console.WriteLine("Hello, World!");
+                    break;
+
+                case "french":
+                    Console.WriteLine("Bonjour le monde!!!");
+                    break;
+
+                case "german":
+                    Console.WriteLine("Hallo Welt!!!");
+                    break;
+
+                case "spanish":
+                    Console.WriteLine("¡¡¡Hola Mundo!!!");
+                    break;
+                }
+
+                Console.Write("Would you like to choose a new entry? (Y/N) ");
+                var repeat = Console.ReadLine();
+                repeat = repeat.ToLower();
+
+                if (repeat == "y")
+                {
+                    continue;
+                }
+                else
+                {
+                    again = false;
+                }
+            }
         }
     }
 }
