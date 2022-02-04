756024e450e9480d0a21eb0cd14c4080d2f6a782
Jay <biskyniffer@gmail.com>
2022-02-03 20:49:34 -0500

    Exercise 13

---
+++ Practice Stuff/Exercise 13/Exercise 13/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var again = true;
+
+            while (again)
+            {
+                Console.Write("Please enter an integer greater than 0: ");
+                int input = int.Parse(Console.ReadLine());
+
+                for (int i = input; i >= 0; i--)
+                {
+                    Console.Write(i + " ");
+                }
+
+                Console.Write("\nWould you like to go again? (y/n)");
+                var repeat = Console.ReadLine().ToLower();
+
+                if (repeat == "y")
+                {
+                    continue;
+                }
+
+                else
+                {
+                    again = false;
+                }
+            }
+        }
+    }
+}
