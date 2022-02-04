7aa357fac2ba8a96285d64cf67544bafc41012e3
Jay <biskyniffer@gmail.com>
2022-02-03 20:32:37 -0500

    Exercise 11

---
+++ Practice Stuff/Exercise 11/Exercise 11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var again = true;
+
+            while (again)
+            {
+                for ( int i = 0; i < 10; i++)
+                {
+                    Console.Write(i + " ");
+                }
+
+                Console.Write("\nWould you like to go again? (y/n)");
+                var repeat = Console.ReadLine().ToLower();
+
+                if (repeat == "y")
+                {
+                    continue;
+                }
+                
+                else
+                {
+                    again = false;
+                }
+            }
+        }
+    }
+}
