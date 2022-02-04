68388e03e011b7266bba2d8e9141304125fd2bdb
Jay <biskyniffer@gmail.com>
2022-02-03 21:06:59 -0500

    Exercise 16

---
+++ Practice Stuff/Exercise 16/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i < 10; i++)
+            {
+                string triangle = new string('*', i);
+                Console.WriteLine(triangle);
+            }
+
+        }
+    }
+}
