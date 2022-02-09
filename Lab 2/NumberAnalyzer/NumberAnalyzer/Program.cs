359459dfe2c3ec0e1ac26b8ed8501ee85156e7bd
Jay <biskyniffer@gmail.com>
2022-02-08 19:23:09 -0500

    Lab 3

--- Lab 2/NumberAnalyzer/NumberAnalyzer/Program.cs
+++ Lab 2/NumberAnalyzer/NumberAnalyzer/Program.cs
@@ -13,26 +13,20 @@ namespace NumberAnalyzer
                 Console.Write("Hello! Please enter an integer between 1 and 100: ");
                 var input = int.Parse(Console.ReadLine());
 
-                if (input %2 != 0 && input <60)
-                {
+                if (input % 2 != 0 && input < 60)
                     Console.WriteLine(input + " is odd and less than 60.");
-                }
-                else if (input %2 == 0 && input <25)
-                {
+                else if (input % 2 == 0 && input < 25)
                     Console.WriteLine(input + " is even and less than 25.");
-                }
-                else if (input %2 == 0 && input >25 && input <61)
-                {
+                else if (input % 2 == 0 && input > 25 && input < 61)
                     Console.WriteLine(input + " is even and between 26 and 60 inclusive.");
-                }
-                else if (input %2 == 0 && input >60)
-                {
+                else if (input % 2 == 0 && input > 60)
                     Console.WriteLine(input + " is even and greater than 60.");
-                }
-                else if (input %2 != 0 && input >60)
-                {
+                else if (input % 2 != 0 && input > 60)
+                    Console.WriteLine("I added this");
                     Console.WriteLine(input + " is odd and greater than 60.");
-                }
+                
+
+
 
                 Console.Write("Would you like to enter another number? (y/n) ");
                 var repeat = Console.ReadLine().ToLower();
---
+++ Lab 3/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+
+namespace Lab_3
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var repeat = true;
+            while (repeat)
+            {
+                Console.Write("Welcome to Dice Land!!! You may roll 2 dice. \nPlease choose how many sides the dice will have (Integers larger than 0 please): ");
+                var input = Console.ReadLine();
+
+                int sides;
+                bool number = int.TryParse(input, out sides);
+
+                if (number)
+                {
+                    Console.WriteLine($"You have chose to roll 2 dice with {sides} sides!");
+                    if (sides == 6)
+                    {
+                        var output = Roller6();
+                        Console.WriteLine(output);
+                    }
+
+                    else
+                    {
+                        var output = Roller(sides);
+                        Console.WriteLine(output);
+                    }
+
+                }
+                else
+                    Console.WriteLine("I'm sorry, that isn't a valid entry.");
+
+                Console.Write("Would you like to try again? (y/n) ");
+                var again = Console.ReadLine().ToLower();
+
+                if (again == "y")
+                    continue;
+                else
+                    repeat = false;
+
+            }
+
+        }
+       
+        public static string Roller(int sides)
+        {
+            var rand = new Random();
+            var res1 = rand.Next(1, sides + 1);
+            var res2 = rand.Next(1, sides + 1);
+            string output = $"{res1} and {res2} make {res1 + res2}.";
+            return output;
+        }
+        public static string Roller6()
+        {
+            var rand = new Random();
+            var res1 = rand.Next(1, 7);
+            var res2 = rand.Next(1, 7);
+            string output;
+
+            if (res1 == 1 && res2 == 1)
+                output = "Double 1s!!! That's Snake Eyes!!! Craps!!!";
+            else if ((res1 == 1 && res2 == 2) || (res1 == 2 && res2 == 1))
+                output = "A 1 and a 2, that's Ace Deuce!!! Craps!!!";
+            else if (res1 == 6 && res2 == 6)
+                output = "Double sixes!!! Box Cars!!! Craps!!!";
+            else if (res1 + res2 == 7)
+                output = $"{res1} and {res2}, 7 wins!!!!";
+            else if (res1 + res2 == 11)
+                output = $"{res1} and {res2}, 11 wins!!!!";
+            else
+                output = $"{res1} and {res2} make {res1 + res2}.";
+            
+
+            return output;
+        }
+
+    }
+}
