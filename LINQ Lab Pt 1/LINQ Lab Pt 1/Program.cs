f2ebfa6d62e4b6d05bc58cce77f91fbafa0c9120
Jay <biskyniffer@gmail.com>
2022-02-23 22:02:17 -0500

    LINQ Pt 1

---
+++ LINQ Lab Pt 1/LINQ Lab Pt 1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+int[] nums = { 10, 2330, 112233, 10, 949, 3764, 2942 };
+
+Console.WriteLine(nums.Min());
+Console.WriteLine(nums.Max());
+Console.WriteLine(nums.Where(x => x<1000).Max());
+Console.WriteLine(string.Join(" ",nums.Where(x => x>10 && x<100).DefaultIfEmpty()));
+Console.WriteLine(string.Join(" ", nums.Where(x => x > 100000 && x < 999999).DefaultIfEmpty()));
+Console.WriteLine(nums.Where(x => x%2 == 0).Count());
+
