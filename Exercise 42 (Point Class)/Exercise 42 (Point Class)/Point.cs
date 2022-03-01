ea44aed5c26b837263c2c037e8ff4a81e8ac8720
Jay <biskyniffer@gmail.com>
2022-02-28 21:20:27 -0500

    Exercise 42

---
+++ Exercise 42 (Point Class)/Exercise 42 (Point Class)/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_42__Point_Class_
+{
+    internal class Point
+    {
+        public double X;
+        public double Y;
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
